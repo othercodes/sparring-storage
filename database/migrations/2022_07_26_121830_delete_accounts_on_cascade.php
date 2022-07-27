b653208ed714d43e7d90c2cefbbe15037b6b92b0
Sergio Palacio Ródenas <sergiopalaciobach@gmail.com>
2022-07-27 14:15:24 +0200

    Feature/Accounts Model

---
+++ database/migrations/2022_07_26_121830_delete_accounts_on_cascade.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->foreignUuid('parent_id')
+                ->nullable()
+                ->references('id')
+                ->on('accounts')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropForeign(["parent_id"]);
+            $table->dropColumn("parent_id");
+        });
+    }
+};
---
+++ database/seeders/AccountsSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use Illuminate\Database\Seeder;
+
+class AccountsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $distributor = Account::factory()->create([
+            'type' => 'distributor',
+            'name' => fake()->company(),
+            'email' => 'distributor@sparring.catchall.infra.im'
+        ]);
+
+        foreach (range(1, 5) as $i) {
+            Account::factory(5)->create([
+                'parent_id' => $distributor->id
+            ]);
+
+            $reseller = Account::factory()->create([
+                'type' => 'reseller',
+                'parent_id' => $distributor->id,
+                'email' => "reseller_{$i}@sparring.catchall.infra.im"
+            ]);
+
+            Account::factory(10)->create([
+                'parent_id' => $reseller->id
+            ]);
+        }
+    }
+}

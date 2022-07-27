8df4599b8c0a33d74f1d7c2f41b368a65d81d165
Sergio Palacio Ródenas <sergiopalaciobach@gmail.com>
2022-07-27 16:42:42 +0200

    Feature/Plan Subscriptions (#5)

---
+++ app/Models/Subscription.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\WithUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+final class Subscription extends Model
+{
+    use HasFactory, WithUuid;
+
+    protected $keyType = 'uuid';
+    public $incrementing = false;
+
+    /**
+     * Properties:
+     * - account_id
+     * - plan_sku
+     * - quantity
+     * - status
+     * - subscribed_at
+     */
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class, 'account_id');
+    }
+
+    /**
+     * A Subscription
+     * @return BelongsTo
+     */
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class, 'plan_sku', 'sku');
+    }
+}

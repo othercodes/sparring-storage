2b99529a392bfabe3323111d8f2546231a9088ef
Unay Santisteban <usantisteban@othercode.es>
2022-07-27 14:32:01 +0200

    Feature/Account Model (#4)
    * Refactor/Change WithUuid key type + incrementing.
    * Feature/Accounts Model.
    
    Co-authored-by: Sergio Palacio Ródenas <sergiopalaciobach@gmail.com>

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -2,13 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Account;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory
  */
-class UserFactory extends Factory
+class AccountFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -23,6 +24,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'type' => 'customer'
         ];
     }
 

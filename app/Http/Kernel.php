4f8a8e644b06fb949486a337786dd5bb3330c91b
Sergio Palacio Ródenas <sergiopalaciobach@gmail.com>
2022-08-01 13:06:29 +0200

    Feature/Passport

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\GetUserFromToken;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 class Kernel extends HttpKernel
 {
@@ -63,5 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'client' => CheckClientCredentials::class,
+        'account' => GetUserFromToken::class
     ];
 }
